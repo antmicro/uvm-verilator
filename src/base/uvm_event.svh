ff4b943e7671d5c2b6671ff03f43cdad1b101058
Ryszard Rozak <rrozak@antmicro.com>
2023-03-01 15:32:43 +0100

    TIMING: Remove problems with event assignments
    uvm-verilator/src/base/uvm_event.svh:189:11: Unsupported: assignment of event data type
      189 |   m_event = e;
          |           ^
    
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- src/base/uvm_event.svh
+++ src/base/uvm_event.svh
@@ -185,7 +185,7 @@ virtual class uvm_event_base extends uvm_object;
 		event e;
 		if (wakeup)
 			->m_event;
-		m_event = e;
+
 		num_waiters = 0;
 		on = 0;
 		trigger_time = 0;
@@ -233,7 +233,7 @@ virtual class uvm_event_base extends uvm_object;
 		super.do_copy(rhs);
 		if(!$cast(e, rhs) || (e==null)) return;
 
-		m_event = e.m_event;
+
 		num_waiters = e.num_waiters;
 		on = e.on;
 		trigger_time = e.trigger_time;
