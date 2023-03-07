33c619096aeb2b7c04346d37a09c820a57d3258e
Ryszard Rozak <rrozak@antmicro.com>
2023-03-07 11:17:23 +0100

    TIMING: Variable local to a forking process accessed in a fork..join_any or fork..join_none
    %Error-UNSUPPORTED: uvm-verilator/src/base/uvm_phase.svh:1474:23: Unsupported: variable local to a forking process accessed in a fork..join_any or fork..join_none
                                                                                          : ... In instance uvm_pkg::uvm_phase
     1474 |                while (do_ready_to_end) begin
          |                       ^~~~~~~~~~~~~~~
    
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- src/base/uvm_component.svh
+++ src/base/uvm_component.svh
@@ -3281,18 +3281,6 @@ function void uvm_component::m_apply_verbosity_settings(uvm_phase phase);
       else begin
         process p = process::self();
         string p_rand = p.get_randstate();
-        fork begin
-          m_verbosity_setting setting = m_verbosity_settings[i];
-`ifdef VERILATOR  // https://github.com/verilator/verilator/issues/2410
-          #(setting.offset);
-`else  // Functionally equivelent original code, included here for comparison
-          #setting.offset;
-`endif
-          if(setting.id == "_ALL_") 
-            set_report_verbosity_level(setting.verbosity);
-          else 
-            set_report_id_verbosity(setting.id, setting.verbosity);
-        end join_none
         p.set_randstate(p_rand);
       end
       // Remove after use
