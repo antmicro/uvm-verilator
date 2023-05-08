1d15084b8d9a859e11d4f6db8f69d1fefd87fb6f
Ryszard Rozak <rrozak@antmicro.com>
2023-05-08 13:26:00 +0200

    Simplification
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- src/base/uvm_text_tr_stream.svh
+++ src/base/uvm_text_tr_stream.svh
@@ -38,46 +38,5 @@ class uvm_text_tr_stream extends uvm_tr_stream;
    // Internal reference to the text-based backend
 
    
-   `uvm_object_utils_begin(uvm_text_tr_stream)
-   `uvm_object_utils_end
-
-   // Function: new
-   // Constructor
-   //
-   // Parameters:
-   // name - Instance name
-   function new(string name="unnamed-uvm_text_tr_stream"); endfunction : new
-
-   // Group: Implementation Agnostic API
-
-   // Function: do_open
-   // Callback triggered via <uvm_tr_database::open_stream>.
-   //
-   // @uvm-accellera The details of this API are specific to the Accellera implementation, and are not being considered for contribution to 1800.2
-   protected virtual function void do_open(uvm_tr_database db,
-                                           string scope,
-                                           string stream_type_name); endfunction : do_open
-
-   // Function: do_close
-   // Callback triggered via <uvm_tr_stream::close>.
-   //
-   // @uvm-accellera The details of this API are specific to the Accellera implementation, and are not being considered for contribution to 1800.2
-   protected virtual function void do_close(); endfunction : do_close
-      
-   // Function: do_free
-   // Callback triggered via <uvm_tr_stream::free>.
-   //
-   // @uvm-accellera The details of this API are specific to the Accellera implementation, and are not being considered for contribution to 1800.2
-   protected virtual function void do_free(); endfunction : do_free
-   
-   // Function: do_open_recorder
-   // Marks the beginning of a new record in the stream
-   //
-   // Text-backend specific implementation.
-   //
-   // @uvm-accellera The details of this API are specific to the Accellera implementation, and are not being considered for contribution to 1800.2
-   protected virtual function uvm_recorder do_open_recorder(string name,
-                                                           time   open_time,
-                                                           string type_name); endfunction : do_open_recorder
 
 endclass : uvm_text_tr_stream
