1bc9fb72c41777b2a1d2c8cd3ce20503d10bc4ee
Ryszard Rozak <rrozak@antmicro.com>
2023-06-20 16:19:08 +0200

    Add workaround for problems with linking of m_t_inst
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- src/base/uvm_callback.svh
+++ src/base/uvm_callback.svh
@@ -686,7 +686,12 @@ class uvm_callbacks #(type T=uvm_object, type CB=uvm_callback)
         if($cast(o,obj)) begin
           uvm_queue#(uvm_callback) qr;
 	  void'(uvm_callbacks#(uvm_report_object, uvm_callback)::get());
+`ifdef VERILATOR
+          // It is a field of uvm_typed_callbacks, which is a base class of uvm_callbacks
+          qr = uvm_typed_callbacks#(uvm_report_object)::m_t_inst.m_tw_cb_q;
+`else
           qr = uvm_callbacks#(uvm_report_object,uvm_callback)::m_t_inst.m_tw_cb_q;
+`endif
           for(int i=0; i<qr.size(); ++i)
               q.push_back(qr.get(i)); 
         end
