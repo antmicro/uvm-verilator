76409378c53e148b5de7da1d7c4516b176bc3985
Ryszard Rozak <rrozak@antmicro.com>
2023-07-12 19:06:52 +0200

    Remove the parameter
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- src/base/uvm_callback.svh
+++ src/base/uvm_callback.svh
@@ -870,7 +870,7 @@ class uvm_callbacks #(type T=uvm_object, type CB=uvm_callback)
 
   // @uvm-ieee 1800.2-2017 auto 10.7.2.4.1
   static function CB get_first (ref int itr, input T obj);
-    CB cb;
+    uvm_callback cb;
      bit a = $cast(cb, null) && cb.callback_mode();
     return null;
   endfunction
