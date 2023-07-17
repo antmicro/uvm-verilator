106cc4ab0eab7dced84b193fa70d15e410f6c159
Ryszard Rozak <rrozak@antmicro.com>
2023-07-17 10:51:23 +0200

    Variable's lifetime is unknown
    %Error: Internal Error: /path/uvm-verilator/src//seq/uvm_sequencer_base.svh:1393:25: ../V3Fork.cpp:194: Variable's lifetime is unknown. Can't determine if a capture is necessary.
    
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- src/seq/uvm_sequencer_base.svh
+++ src/seq/uvm_sequencer_base.svh
@@ -1390,10 +1390,10 @@ function void uvm_sequencer_base::start_phase_sequence(uvm_phase phase);
     w.pid = process::self();
     w.seq = seq;
     w.pid.srandom(uvm_create_random_seed(seq.get_type_name(), this.get_full_name()));
-    m_default_sequences[phase] = w;
+
     // this will either complete naturally, or be killed later
     seq.start(this);
-    m_default_sequences.delete(phase);
+
   end
   join_none
 
