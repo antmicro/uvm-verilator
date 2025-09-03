57745f67cf4fe2aa22b377ef021596d16a2e1657
Accellera <>
2025-09-03 09:50:07 +0200

    UVM 2020
    Signed-off-by: Krzysztof Bieganski <kbieganski@antmicro.com>

--- src/macros/uvm_phase_defines.svh
+++ src/macros/uvm_phase_defines.svh
@@ -2,11 +2,11 @@
 `define UVM_PHASE_DEFINES_SVH
 //
 //----------------------------------------------------------------------
-// Copyright 2007-2011 Mentor Graphics Corporation
-// Copyright 2010-2011 Synopsys, Inc.
-// Copyright 2007-2018 Cadence Design Systems, Inc.
 // Copyright 2011 AMD
-// Copyright 2018 NVIDIA Corporation
+// Copyright 2007-2018 Cadence Design Systems, Inc.
+// Copyright 2007-2011 Mentor Graphics Corporation
+// Copyright 2018-2024 NVIDIA Corporation
+// Copyright 2010-2011 Synopsys, Inc.
 //   All Rights Reserved Worldwide
 //
 //   Licensed under the Apache License, Version 2.0 (the
@@ -23,6 +23,16 @@
 //   the License for the specific language governing
 //   permissions and limitations under the License.
 //----------------------------------------------------------------------
+
+//----------------------------------------------------------------------
+// Git details (see DEVELOPMENT.md):
+//
+// $File$
+// $Rev$
+// $Hash$
+//
+//----------------------------------------------------------------------
+
 
 
 // uvm_root.svh uses these macros to simplify creation of all the phases.
